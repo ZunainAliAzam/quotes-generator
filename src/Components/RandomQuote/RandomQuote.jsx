fbb0a27099fd71add19e72564e9416dc7cbb3317
ZunainAliAzam <zunainazam456@gmail.com>
2024-01-29 03:29:22 +0500

    twitter post updated

--- src/Components/RandomQuote/RandomQuote.jsx
+++ src/Components/RandomQuote/RandomQuote.jsx
@@ -1,33 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./RandomQuote.css";
 import twitter_icon from "../Assets/x.png";
 import refresh_icon from "../Assets/refresh.png";
 
 const RandomQuote = () => {
-  let quotes=[];
-
-  async function loadQuotes(){
-    const response = await fetch("https://type.fit/api/quotes")
-    quotes = await response.json();
-  }
-
+  const [quotes, setQuotes] = useState([]);
   const [quote, setQuote] = useState({
     text: "Difficulties increase the nearer we get to the goal.",
-    Author: "Johann Wolfgang Von Goethe",
+    author: "Johann Wolfgang Von Goethe",
   });
-    const random = () => {
-      const select = quotes[Math.floor(Math.random()*quotes.length)];
+
+  useEffect(() => {
+    async function loadQuotes() {
+      const response = await fetch("https://type.fit/api/quotes");
+      const quotesData = await response.json();
+      setQuotes(quotesData);
     }
-    
+
+    loadQuotes();
+  }, []);
+
+  const random = () => {
+    const selectedQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(selectedQuote);
+  };
+  const twitterPost = () => {
+    window.open(
+      `https://twitter.com/intent/tweet?text=${quote.text} - ${
+        quote.author.split(",")[0]
+      }`
+    );
+  };
+
   return (
     <div className="container">
       <div className="quote">"{quote.text}"</div>
       <div className="line"></div>
       <div className="bottom">
-        <div className="author">{quote.Author}</div>
+        <div className="author">{quote.author.split(",")[0]}</div>
         <div className="icons">
-          <img className="img-1" src={refresh_icon} alt="refresh-icon" />
-          <img className="img-2" src={twitter_icon} alt="x-icon" />
+          <img
+            className="img-1"
+            src={refresh_icon}
+            onClick={random}
+            alt="refresh-icon"
+          />
+          <img
+            className="img-2"
+            src={twitter_icon}
+            onClick={() => {
+              twitterPost();
+            }}
+            alt="x-icon"
+          />
         </div>
       </div>
     </div>
